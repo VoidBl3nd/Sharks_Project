d4e65adb854e7b127ba94f0b8aff348458495ec6
VoidBl3nd <py.coderunner@gmail.com>
2023-12-02 16:46:57 +0100

    Simplify departure country selection

--- backend/plotly_event.py
+++ backend/plotly_event.py
@@ -3,6 +3,7 @@ import streamlit as st
 from typing import Set
 from typing import Dict
 import plotly.express as px
+import plotly.graph_objects as go
 from streamlit_plotly_events import plotly_events
 
 df =pd.read_csv('input_data/plotly_countries_and_codes.xls').filter(['COUNTRY','CODE'])
@@ -24,8 +25,16 @@ def build_plotly_chart(df):
                         color = 'selected',
                         hover_name="COUNTRY",
                         template = 'seaborn',
-                        #projection="orthographic"
+                        color_discrete_sequence= ['#76b0a6', 'rgb(255,255,179)'],
+                        projection="orthographic",
+                        hover_data= {'selected': False, 'CODE':False}
     )
+    fig.update_geos(showcoastlines=True, coastlinecolor = '#afe3e0',
+                    showland=True, landcolor='White',
+                    showocean=True, oceancolor= '#50545c',
+                    showlakes=False, )#Otherwise lake are showing white
+    fig.update_layout(geo=dict(bgcolor= 'rgba(0,0,0,0)')) #Color when zoom out of map
+    fig.add_trace(go.Choropleth(locations=['ATA'],z=[0],colorscale=[[0,'#76b0a6'], [1,'#76b0a6']],showlegend=False,showscale=False)) #color Antartica
 
     #Make it kind to the eye in Streamlit
     #---------------------------------------------------
@@ -34,123 +43,29 @@ def build_plotly_chart(df):
 
     return fig
 
-def render_plotly_chart(df, debug_st = False, selection_menu = False):
-    #Filter Previously selected points
+def render_plotly_chart(df):
+    #Select a country (only if no country yet selected)
     #---------------------------------------------------
-    if '_selected_country_' in st.session_state:
-        if debug_st:
-            st.write('----- Lastly Selected country --------')
-            st.write("country: ", st.session_state['_selected_country_'])
-        df.loc[df.COUNTRY == st.session_state['_selected_country_'], 'selected'] = True
-
-    #Render the plot & capture selection
-    #---------------------------------------------------
-    fig = build_plotly_chart(df)
-    map_selection = plotly_events(fig, select_event=True, key=f"_country_selector_")
-    if debug_st:
-        print(map_selection)
-
-    #If rerun, do not change the selected value (given that we change the df_selectable indexes due to coloring but don't change the selected index from plotlu event)
-    #---------------------------------------------------
-    if st.session_state['_due_to_rerun_'] or st.session_state['_widget_rerun_']:
-        selected_country = st.session_state['_selected_country_']
-    else:
-        selected_country = 0
-
-    #If run not due to forced rerun,
-    #---------------------------------------------------
-    try:
-        if map_selection[0]['curveNumber'] == 0 and not st.session_state['_due_to_rerun_']:
-            df_selectable = df.drop(df[df.selected == True].index).reset_index(drop = True) #Isolate selectable indexes (for curveNumber = 0)
+    if '_selected_country_' not in st.session_state or st.session_state['_selected_country_'] == 0:
+        fig = build_plotly_chart(df)
+        map_selection = plotly_events(fig, select_event=True, key=f"_country_selector_")
+        if map_selection != []:
             selected_index = map_selection[0]['pointNumber']
-            selected_country = df_selectable.iloc[selected_index].COUNTRY
-
-            if st.session_state['_curve_number_'] == map_selection[0]['curveNumber'] \
-            and st.session_state['_selected_index_'] == selected_index \
-            and df.selected.sum() > 0 :    
-                selected_country = 0
-
-            st.session_state['_selected_index_'] = map_selection[0]['pointNumber']
-            st.session_state['_curve_number_'] = map_selection[0]['curveNumber']
-    except:
-        pass #no selection made
-
-    if st.session_state['_due_to_rerun_'] or st.session_state['_widget_rerun_']:
-        if st.session_state['_selected_country_'] == 0:
-            country_to_display = None
+            st.session_state['_selected_country_'] = df.iloc[selected_index].COUNTRY
+            need_update = True
         else:
-            country_to_display = int(df[df.COUNTRY == st.session_state['_selected_country_']].index.values[0])
+            need_update = False
 
-        if selection_menu:
-            picked_country = st.selectbox('Sélectionner un pays de départ',df.COUNTRY.tolist(), index = country_to_display, placeholder='Choisir un pays ou le sélectionner sur la carte')
-            if debug_st:
-                print('picked country :',picked_country)
-                print('selected country : ', selected_country)
-            if picked_country !=  None and picked_country != selected_country: #TO REVIEW
-                selected_country = picked_country
-                st.session_state['_curve_number_'] = 0
-                if debug_st:
-                    print('selected-------------------------------------------------------------------------')
-        if country_to_display == None:
-            chosen_country = ''
-        else:
-            chosen_country = df.COUNTRY.tolist()[country_to_display]
-        #if picked_country
-        #selected_country =     
+    #Display selection
+    #---------------------------------------------------
     else:
-        chosen_country = ''
-    if debug_st:
-        print("---->", chosen_country)
+        selected_country = st.session_state['_selected_country_']
+        df.loc[df.COUNTRY == selected_country, 'selected'] = True
+        fig = build_plotly_chart(df)
+        st.plotly_chart(fig)
+        need_update = False
 
-
-    #else:  
-    #    picked_country = st.selectbox('Sélectionner un pays de départ',df.COUNTRY, index = None, placeholder='Choisir un pays ou le sélectionner sur la carte')
-    #    st.session_state['_selected_country_'] = picked_country
-    if debug_st:
-        st.write("Returned selection object: ", map_selection)
-        st.write("Previous selected country: ", st.session_state['_selected_country_'])
-        st.write("Currently selected country: ", st.session_state['_selected_country_'])
-
-    return selected_country, chosen_country
-
-def update_state(selected_country, debug_st = False):
-    """
-    - Selection triggers run from top to bottom.
-    - Value selected is only returned during the rerun
-    - If value selected, need to store it and rerun 
-    """
-    #If selected something during this run, rerun to make updates :)
-    if debug_st:
-        print('current value: ',selected_country)
-        print('previous value: ' ,st.session_state['_selected_country_'])
-    try:
-        if selected_country != st.session_state['_selected_country_']:
-            st.session_state['_selected_country_'] = selected_country
-            st.session_state['_due_to_rerun_'] = 1
-            st.rerun()
-            
-        else:
-            st.session_state['_due_to_rerun_'] = 0
-    except Exception as e:
-        if debug_st:
-            print('EXCEPTION 2 ')
-            print(f'{e}')
-        st.session_state['_selected_country_'] = selected_country
-        st.session_state['_due_to_rerun_'] = 1
-        st.rerun()
-
-def notify_rerun():
-    st.session_state['_widget_rerun_'] = 1
-    print('WIDGET-----------------------------------')
+    return need_update
         
 if __name__ == "__main__":
-    print('-----------------------------------------------------')
-    initialize_state()
-    selected_country, chosen_country = render_plotly_chart(df)
-
-    print('due_to_rerun : ', st.session_state['_due_to_rerun_'])
-    if st.session_state['_due_to_rerun_']:
-        st.warning(f"RERUN { str(st.session_state['_due_to_rerun_'])}")
-
-    update_state(selected_country)
-    #Quand met en couleur, change l'index ! 
+    print('Please launch in Streamlit')
--- pages/HOME.py
+++ pages/HOME.py
@@ -4,26 +4,39 @@ import plotly.express as px
 from datetime import datetime
 
 from backend.Sharks_streamlit_utils_v1 import get_session_state, extract_popular_activities
-from backend.plotly_event import initialize_state, build_plotly_chart, render_plotly_chart, update_state, notify_rerun
+from backend.plotly_event import initialize_state, build_plotly_chart, render_plotly_chart
 
 df_countries =pd.read_csv('input_data/plotly_countries_and_codes.xls').filter(['COUNTRY','CODE']).assign(selected = False)
 var_list = get_session_state(['transformed_data/sharks', 'transformed_data/activities_words'])
 sharks, activities = var_list[0], var_list[1]
 
+#Initialize Streamlit
+st.set_page_config(page_title="Sharky cruise builder", layout = "centered", page_icon= '🦈') # must happen before any streamlit code /!\
+st.markdown('<style>div.block-container{padding-top:3rem;}</style>', unsafe_allow_html=True) # remove blank top space
+
 #Select departure country
 with st.expander('Departure country',expanded = True):
     st.header(':blue[Step 1 :] Select the depature country')
     initialize_state()
-    selected_country, chosen_country = render_plotly_chart(df_countries, debug_st = False)
-    update_state(selected_country, debug_st =False)
-    st.markdown('Chosen country :')
-    st.markdown(chosen_country)
+    need_update = render_plotly_chart(df_countries)
+    if need_update:
+        st.rerun()
+
+    c01, c02, c03 = st.columns(3)
+    if st.session_state['_selected_country_'] == 0:
+        c01.info('Please select a country')
+    else:
+        c01.info('Country selected :')
+        c02.success(f"**{st.session_state['_selected_country_']}**")
+        if c03.button('Pick another country', type = 'primary'): 
+            st.session_state['_selected_country_'] = 0
+            st.rerun()
 
 #with st.expander('Cruise parameters',expanded = True):
 st.header(':blue[Step 2 :] Tweak cruise  parameters')
 c1,c2,c3 = st.columns([15,2,40])
 popular_activites = extract_popular_activities(activities)
-c1.number_input('Cruise duration', min_value=1, placeholder= 'Duration in days',value = None, on_change=notify_rerun)
+c1.number_input('Cruise duration', min_value=1, placeholder= 'Duration in days',value = None)
 c1.multiselect('Activities', options= popular_activites.Activity.unique().tolist())
 #c3.slider('Historical attacks period (included)',min_value= int(sharks.date.dt.year.min()), max_value= int(sharks.date.dt.year.max()), value = [2000,2019])
 period_start = c1.date_input('Historical Period start', min_value= datetime(int(sharks.date.dt.year.min()),1,1), max_value= datetime(int(sharks.date.dt.year.max()),1,1), value = datetime(2000,1,1), format = 'DD-MM-YYYY')
@@ -42,7 +55,4 @@ with c3.expander('View historical data distribution:'):
 
     
     st.markdown(':orange[**Note**: *Only attacks in the selected period range will be showed across the application*]')
-
-
-st.session_state['_widget_rerun_'] = 0
 
